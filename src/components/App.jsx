04921461aa96ee4176cdf6d31bf8417a4fc16c79
Modesto <mofesdo.dev@gmail.com>
2025-05-26 15:23:31 -0700

    Feat: Added a redirect catch all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Routes>
       <Route path="/ducks" element={<Ducks />} />
@@ -26,6 +29,16 @@ function App() {
           </div>
         }
       />
+      <Route
+        path="*"
+        element={
+          isLoggedIn ? (
+            <Navigate to="/ducks" replace />
+          ) : (
+            <Navigate to="/login" replace />
+          )
+        }
+      ></Route>
     </Routes>
   );
 }
